998a71edfbdd4a3742ec55a0821c9ee906fbc71a
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2016-10-28 17:09:31 +0100

    Can add sprites both the player and enemy sprute collections

--- engine/models/animation.js
+++ engine/models/animation.js
@@ -5,4 +5,16 @@ const Animation = function(){
 
 }
 
+Animation.prototype = {
+
+  addPlayerSprites: function(sprites, action){
+    this.playerSprites.push({action: action, sprites: sprites});
+  },
+
+  addEnemySprites: function(sprites, action, enemy){
+    this.enemySprites.push({enemy: enemy, action: action, sprites: sprites})
+  }
+
+}
+
 module.exports = Animation;
--- engine/models/engine_specs/animation_specs.js
+++ engine/models/engine_specs/animation_specs.js
@@ -17,4 +17,14 @@ describe("Game has, ", function(){
     assert.deepEqual([], animation.enemySprites);
   })
 
+  it("can add player sprites", function(){
+    animation.addPlayerSprites(["img1", "img2"], "walking")
+    assert.deepEqual([{action: "walking", sprites: ["img1", "img2"]}], animation.playerSprites);
+  })
+
+  it("can add enemy sprites", function(){
+    animation.addEnemySprites(["img 1", "img 2"], "walking", "enemy 1");
+    assert.deepEqual([{enemy: "enemy 1", action: "walking", sprites: ["img 1", "img 2"]}], animation.enemySprites);
+  })
+
 })
