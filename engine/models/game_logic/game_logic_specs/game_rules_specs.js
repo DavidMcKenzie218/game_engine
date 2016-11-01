15bfb9891acee2d0553f296aef4182bb8ddc1d29
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2016-11-01 10:18:48 +0000

    A boxer object can be the winner

--- engine/models/game_logic/game_logic_specs/game_rules_specs.js
+++ engine/models/game_logic/game_logic_specs/game_rules_specs.js
@@ -1,20 +1,37 @@
 const assert = require('assert');
 const Game = require('../game_rules.js');
+const Boxer = require('../boxer.js');
 
 describe("Game", function(){
 
   let game;
+  let player;
 
   before(function(){
     game = new Game();
+    player = new Boxer();
   });
+
+  beforeEach(function(){
+    game.reset();
+  })
 
   it("has no winner", function(){
     assert.equal(false, game.isWinner);
   })
 
   it("can have a player as a winner", function(){
-    assert.equal(null, game.winner)
+    assert.equal(null, game.winner);
+  })
+
+  it("has a winner", function(){
+    game.setWinner();
+    assert.equal(true, game.isWinner);
+  })
+
+  it("has a winning player", function(){
+    game.setWinner(player);
+    assert.equal(player, game.winner);
   })
 
 })
--- engine/models/game_logic/game_rules.js
+++ engine/models/game_logic/game_rules.js
@@ -1,6 +1,22 @@
 const GameRules = function(){
+
   this.isWinner = false;
   this.winner = null;
+
+}
+
+GameRules.prototype = {
+
+  reset: function(){
+    this.isWinner = false;
+    this.winner = null;
+  },
+
+  setWinner: function(player){
+    this.isWinner = true;
+    this.winner = player;
+  }
+
 }
 
 module.exports = GameRules;
